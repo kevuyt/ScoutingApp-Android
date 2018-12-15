b15fa2dd11db44e739cb602da9f63d3ae03cc963
Keval Kataria <kevalkataria@gmail.com>
2018-12-15 09:31:51 -0500

    random stuff

--- app/src/main/java/archishmaan/com/scoutingapp/Activities/StashActivity.java
+++ app/src/main/java/archishmaan/com/scoutingapp/Activities/StashActivity.java
@@ -24,7 +24,6 @@ import static archishmaan.com.scoutingapp.Activities.ScoutingActivity.matches;
 public class StashActivity extends Fragment implements View.OnClickListener {
     ScrollView scrollView;
     LinearLayout linearLayout;
-    int dropPoints, markerPoints, autoParkPoints, samplePoints, hangPoints, endPartialParkPoints, endFullParkPoints, autoPoints, teleopPoints, endGamePoints, totalPoints;
 
     @SuppressLint("SetTextI18n")
     @Nullable
@@ -38,32 +37,6 @@ public class StashActivity extends Fragment implements View.OnClickListener {
 
         for (ScoutingModel match : matches) {
             Button button = new Button(getContext());
-            /*
-            if (match.isAutoDrop()) {dropPoints = 30;}
-            else {dropPoints = 0;}
-            if (match.isMarker()) {markerPoints = 15;}
-            else {markerPoints = 0;}
-            if (match.isAutoPark()) {autoParkPoints = 10;}
-            else {autoParkPoints = 0;}
-            if (match.isSample()) {samplePoints = 25;}
-            else {samplePoints = 0;}
-            if (match.isEndHang()) {hangPoints = 50;}
-            else {hangPoints = 0;}
-            if (match.isEndPartial()) {endPartialParkPoints = 15;}
-            else {endPartialParkPoints = 0;}
-            if (match.isFullPark()) {endFullParkPoints = 25;}
-            else {endFullParkPoints = 0;}
-            autoPoints = dropPoints + markerPoints + autoParkPoints + samplePoints;
-            teleopPoints = (match.getDepot() * 2) + (match.getLander() * 5);
-            endGamePoints = hangPoints + endPartialParkPoints + endFullParkPoints;
-            totalPoints = autoPoints + teleopPoints + endGamePoints;
-            button.setText("Match #: " + match.getMatchNumber() + ", Team #: "
-                    + match.getTeamNumber() + ", Auto Points: " + autoPoints
-                    + match.getTeamNumber() + ", Auto Points: " + autoPoints
-                    + ", Tele-op Points: " + teleopPoints + ", End Game Points: "
-                    + endGamePoints + ", Total: " + totalPoints);
-                    match.setTextSize(13);
-                    */
             button.setText ("Match #: " + match.getMatchNumber() + ", Team #: " + match.getTeamNumber());
             button.setId(match.getMatchNumber());
             button.setTextSize(15);
@@ -78,8 +51,6 @@ public class StashActivity extends Fragment implements View.OnClickListener {
     }
 
     @Override
-    public void onClick(View view) {
-    
-    }
+    public void onClick(View view) {}
 
 }
