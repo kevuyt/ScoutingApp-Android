dc8ff2f2aea47cd753ed1472d9f94524f22d3869
Archishmaan Peyyety <archishou@gmail.com>
2018-12-28 16:23:53 -0500

    cleanlisness

--- app/src/main/java/archishmaan/com/scoutingapp/Activities/StashActivity.java
+++ app/src/main/java/archishmaan/com/scoutingapp/Activities/StashActivity.java
@@ -7,22 +7,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-import archishmaan.com.scoutingapp.Models.ScoutingModel;
 import archishmaan.com.scoutingapp.R;
+
 import static archishmaan.com.scoutingapp.Activities.ScoutingActivity.matches;
 
 /**
@@ -33,7 +29,6 @@ public class StashActivity extends Fragment implements View.OnClickListener {
     ScrollView scrollView;
     LinearLayout linearLayout;
     static List<Integer> updateMatch = new ArrayList<>();
-    int buttonIndex;
     @SuppressLint("SetTextI18n")
     @Nullable
     @Override
@@ -64,9 +59,6 @@ public class StashActivity extends Fragment implements View.OnClickListener {
                             .commit();
                 }
             });
-
-
-
         }
 
         return scrollView;
