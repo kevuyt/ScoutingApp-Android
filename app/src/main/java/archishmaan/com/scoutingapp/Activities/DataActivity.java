28768a0c81f815bb893d6017a62b7f9f4bfdf69f
Keval Kataria <kevalkataria@gmail.com>
2019-01-26 11:23:47 -0500

    Update to make it like master sheet

--- app/src/main/java/archishmaan/com/scoutingapp/Activities/DataActivity.java
+++ app/src/main/java/archishmaan/com/scoutingapp/Activities/DataActivity.java
@@ -38,7 +38,7 @@ import static archishmaan.com.scoutingapp.Activities.ScoutingActivity.matches;
 public class DataActivity extends Fragment implements View.OnClickListener {
     ScrollView scrollView;
     int score;
-    int drop, marker, sample, autoPark, hang, partPark, fullPark;
+    String drop, marker, sample, autoPark, hang, endPark;
     //int f= 1;
     int requestValue = 0;
     LinearLayout linearLayout;
@@ -65,7 +65,7 @@ public class DataActivity extends Fragment implements View.OnClickListener {
             if (match.isEndHang()) {score+=50;}
             if (match.isEndPartial()) {score+=15;}
             if (match.isFullPark()) {score+=25;}
-            button.setText ("Match #: " + match.getMatchNumber() + ", Team #: " + match.getTeamNumber() + ", Score: " + score++);
+            button.setText ("Match #: " + match.getMatchNumber() + ", Team #: " + match.getTeamNumber() + ", Score: " + score);
             button.setId(match.getMatchNumber());
             button.setTextSize(15);
             button.setWidth(1000);
@@ -83,6 +83,8 @@ public class DataActivity extends Fragment implements View.OnClickListener {
             });
         }
         Button export = new Button(getContext());
+        export.setId(matches.get(matches.size() - 1).getTeamNumber());
+
         export.setText("Export");
         export.setTextSize(25);
         export.setWidth(1000);
@@ -90,60 +92,58 @@ public class DataActivity extends Fragment implements View.OnClickListener {
         export.getBackground().setColorFilter(Color.parseColor("#DAA620"), PorterDuff.Mode.DARKEN);
         linearLayout.addView(export);
         export.setOnClickListener(v -> {
-            try {
-                String filename = "Scouting Data " + matches.get(0).getTournament() + String.valueOf(matches.get(matches.size() - 1).getMatchNumber())+ ".csv";
 
-                File directoryDocument = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
-                File file = new File(directoryDocument, filename);
-                if (ContextCompat.checkSelfPermission(Objects.requireNonNull(getActivity()), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, requestValue);
-                    requestValue+=1;
-                }
-                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, requestValue);
-                    requestValue+=1;
-                }
+        try {
+            String filename = "Scouting Data " + matches.get(0).getTournament() + String.valueOf(matches.get(matches.size() - 1).getMatchNumber())+ ".csv";
 
-                FileOutputStream outputStream = new FileOutputStream(file);
-                outputStream.write(("Tournament Name, Match Number, Team Number, Auto Drop, Sample, Marker, Auto Park, Depot, Lander, End Hang, End Partial Park, End Full Park" + System.lineSeparator()).getBytes());
-
-                for (ScoutingModel match : matches) {
-
-                    if (match.isAutoDrop()) {drop = 1;}
-                    else {drop = 0;}
-                    if (match.isSample()) {sample = 1;}
-                    else if (match.isDoubleSample()) {sample = 2;}
-                    else {sample = 0;}
-                    if (match.isMarker()) {marker = 1;}
-                    else {marker = 0;}
-                    if (match.isAutoPark()) {autoPark = 1;}
-                    else {autoPark = 0;}
-                    if (match.isEndHang()) {hang = 1;}
-                    else {hang = 0;}
-                    if (match.isEndPartial()) {partPark = 1;}
-                    else {partPark = 0;}
-                    if (match.isFullPark()) {fullPark = 1;}
-                    else {fullPark = 0;}
-                    outputStream.write((match.getTournament() + ", ").getBytes());
-                    outputStream.write((match.getMatchNumber() + ", ").getBytes());
-                    outputStream.write((match.getTeamNumber() + ", ").getBytes());
-                    outputStream.write((String.valueOf(drop) + ", ").getBytes());
-                    outputStream.write((String.valueOf(sample) + ", ").getBytes());
-                    outputStream.write((String.valueOf(marker) + ", ").getBytes());
-                    outputStream.write((String.valueOf(autoPark) + ", ").getBytes());
-                    outputStream.write((match.getDepot() + ", ").getBytes());
-                    outputStream.write((match.getLander() + ", ").getBytes());
-                    outputStream.write((String.valueOf(hang) + ", ").getBytes());
-                    outputStream.write((String.valueOf(partPark) + ", ").getBytes());
-                    outputStream.write((String.valueOf(fullPark) + System.lineSeparator()).getBytes());
-                }
-                outputStream.close();
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
+            File directoryDocument = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+            File file = new File(directoryDocument, filename);
+            if (ContextCompat.checkSelfPermission(Objects.requireNonNull(getActivity()), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, requestValue);
+                requestValue+=1;
             }
-        });
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, requestValue);
+                requestValue+=1;
+            }
+
+            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(("Tournament Name, Match Number, Team Number, Auto Drop, Marker, Auto Park, Sample, Depot, Lander, End Hang, End Park" + System.lineSeparator()).getBytes());
+
+            for (ScoutingModel match : matches) {
+
+                if (match.isAutoDrop()) {drop = "y";}
+                else {drop = "n";}
+                if (match.isSample()) {sample = String.valueOf(25);}
+                else if (match.isDoubleSample()) {sample = String.valueOf(50);}
+                else {sample = "n";}
+                if (match.isMarker()) {marker = "y";}
+                else {marker = "n";}
+                if (match.isAutoPark()) {autoPark = "y";}
+                else {autoPark = "n";}
+                if (match.isEndHang()) {hang = "y";}
+                else {hang = "n";}
+                if (match.isEndPartial()) {endPark = String.valueOf(15);}
+                else if (match.isFullPark()) {endPark = String.valueOf(25);}
+                else {endPark = "n";}
+                outputStream.write((match.getTournament() + ", ").getBytes());
+                outputStream.write((match.getMatchNumber() + ", ").getBytes());
+                outputStream.write((match.getTeamNumber() + ", ").getBytes());
+                outputStream.write((drop + ", ").getBytes());
+                outputStream.write((marker + ", ").getBytes());
+                outputStream.write((autoPark + ", ").getBytes());
+                outputStream.write((sample + ", ").getBytes());
+                outputStream.write((match.getDepot() + ", ").getBytes());
+                outputStream.write((match.getLander() + ", ").getBytes());
+                outputStream.write((endPark + System.lineSeparator()).getBytes());
+            }
+            outputStream.close();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    });
 
         return scrollView;
     }
